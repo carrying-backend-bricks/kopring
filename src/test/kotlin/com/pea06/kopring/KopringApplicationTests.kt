f49e2cfc85de5a007615d74f1adcb9fab3b927b8
pea06 <101560095+pea06@users.noreply.github.com>
2023-03-20 16:46:58 +0900

    :tada: :: start project

---
+++ src/test/kotlin/com/pea06/kopring/KopringApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.pea06.kopring
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KopringApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
